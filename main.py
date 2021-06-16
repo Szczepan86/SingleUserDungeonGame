cce17db7baa902bfc69eeb85ead5fda10ff6e47f
Szczepan86 <szczepanski.mateusz@gmail.com>
2021-06-16 19:15:21 +0200

    World class creation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Room():
+class Room:
     def __init__(self, x, y, name, description):
         self.x = x
         self.y = y
@@ -10,8 +10,25 @@ class Room():
                f'{self.description}'
 
 
+class World:
+    def __init__(self, size):
+        self.world = [[None for y in range(size)] for x in range(size)]
+
+    def add_room(self, room):
+        self.world[room.x][room.y] = room
+
+    def get_room(self, x, y):
+        try:
+            return self.world[x][y]
+        except IndexError:
+            return None
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    room = Room(1, 2, 'Test Room', 'This is my test room and there\'s nothing to do!')
-    print(room)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    world = World(20)
+    newRoom = Room(1, 2, 'Test Room', 'This is my test room and there\'s nothing to do!')
+    world.add_room(newRoom)
+    print(world.get_room(1, 2))
+    print(world.get_room(6, 4))
+    print(world.get_room(999, 999))
